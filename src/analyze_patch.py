7571318a00914f21dbe25063fcf0f6c79ad9d251
Tommaso Tocci <tommaso.tocci@mongodb.com>
2025-03-07 21:31:27 +0100

    Add intial command implementation

---
+++ src/analyze_patch.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python2
+
+import click
+import logging
+import json
+import re
+
+from evergreen import RetryingEvergreenApi
+
+logger = logging.getLogger(__name__)
+
+def setup_trace_logging(trace_requests):
+    logging.getLogger("urllib3").setLevel(logging.DEBUG if trace_requests else logging.WARNING)
+    logging.getLogger("evergreen.api").setLevel(logging.DEBUG if trace_requests else logging.WARNING)
+
+def setup_logging(verbose):
+    logging.basicConfig(level=logging.DEBUG if verbose else logging.INFO)
+    setup_trace_logging(False)
+
+def get_tests_from_patch(evg_api, patch_id,
+        variant_name_pattern = None,
+        suite_name_pattern = None,
+        test_name_pattern = None,
+        skip_inactive=True):
+
+    for build in evg_api.builds_by_version(patch_id):
+        variant_name = build.build_variant
+        if skip_inactive and not build.activated:
+            logger.debug(f"Skipping variant because inactive {variant_name}")
+            continue
+        if variant_name_pattern and not variant_name_pattern.match(variant_name):
+            logger.debug(f"Skipping variant because name does not match {variant_name}")
+            continue
+        for task in build.get_tasks():
+            suite_name = task.display_name
+            if skip_inactive and not task.activated:
+                logger.debug(f"Skipping suite because inactive {suite_name}")
+                continue
+            if suite_name_pattern and not suite_name_pattern.match(suite_name):
+                logger.debug(f"Skipping suite because name does not match {suite_name}")
+                continue
+            if not task.finish_time:
+                raise Exception(f"Encountered one matching suites that is still in progress {suite_name}")
+            for test in task.get_tests():
+                test_name = test.test_file
+                if test_name_pattern and not test_name_pattern.match(test_name):
+                    logger.debug(f"Skipping test  because name does not match {test_name}")
+                    continue
+                yield {
+                    'test_name': test_name,
+                    'variant': variant_name,
+                    'suite': suite_name,
+                    'status': test.status,
+                    'duration': test.duration}
+
+
+@click.group()
+@click.option('-v', '--verbose', 'verbose', is_flag=True, show_default=True, default=False, help='Enable debug logs.')
+def cli(verbose):
+    """
+    CLI utility to operate on evregreen patch test results
+    """
+    setup_logging(verbose)
+
+@cli.command()
+@click.option('-p', '--patch', 'patch_id', required=True, help='The ID of the patch to analyze.')
+@click.option('--filter-variant', 'variant_name_regex', show_default=True, help='Filter variants using the given regular expression.')
+@click.option('--filter-suites', 'suite_name_regex', show_default=True, help='Filter suites using the given regular expression.')
+@click.option('--filter-tests', 'test_name_regex', default=r'.*js$', show_default=True, help='Filter tests using the given regular expression.')
+@click.option('--trace-requests', 'trace_requests', is_flag=True, show_default=True, default=False, help='Trace network request.')
+def get_tests_results(patch_id, variant_name_regex, suite_name_regex, test_name_regex, trace_requests):
+    """
+    Fetch tests results from an evergeen patch
+    """
+    setup_trace_logging(trace_requests)
+    api = RetryingEvergreenApi.get_api(use_config_file=True)
+
+    variant_name_pattern = re.compile(variant_name_regex) if variant_name_regex else None
+    suite_name_pattern = re.compile(suite_name_regex) if suite_name_regex else None
+    test_name_pattern = re.compile(test_name_regex) if test_name_regex else None
+
+    tests_results = {}
+    with api.with_session() as session:
+        for execution_stats in get_tests_from_patch(session, patch_id, variant_name_pattern, suite_name_pattern, test_name_pattern):
+            test_name = execution_stats['test_name']
+            if test_name not in tests_results:
+                tests_results[test_name] = {
+                    'test_name': test_name,
+                    'num_failed': 0,
+                    'num_succeeded': 0,
+                    'executions': [],
+                    }
+            test_result = execution_stats['status']
+            if test_result == "pass":
+                tests_results[test_name]['num_succeeded'] += 1
+            elif test_result == "fail":
+                tests_results[test_name]['num_failed'] += 1
+            else:
+                raise Exception(f'Encountered unexpected test result {test_result} for test {test_name}')
+            tests_results[test_name]['executions'].append(execution_stats)
+    if not tests_results:
+        logger.error("Did not find any matching tests. This could be because the patch is still running or because the requested filters are too strict")
+        raise click.Abort()
+    print(json.dumps(list(tests_results.values())))
+
+
+if __name__ == "__main__":
+    cli()
---
+++ src/viewless_suites.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python2
+
+import click
+import logging
+import re
+import os
+import json
+import yaml
+
+logger = logging.getLogger(__name__)
+MDB_REPO = None
+VIEWLESS_OVERRIDES_PATH = "buildscripts/resmokeconfig/matrix_suites/overrides/viewless_timeseries.yml"
+MAPPING_SUITES_FOLDER = "buildscripts/resmokeconfig/matrix_suites/mappings/"
+OVERRIDE_SECTION_NAME = 'only_validated_timeseries_tests_selector'
+
+def update_validated_viewless_tests(new_roots, force_override=False):
+
+    viewless_override_file = os.path.join(MDB_REPO, VIEWLESS_OVERRIDES_PATH)
+    # Load the YAML file
+    with open(viewless_override_file, 'r') as file:
+        overrides = yaml.safe_load(file)
+
+    # Find and update the "only_validated_timeseries_tests_selector" section
+    for item in overrides:
+        if item.get('name') == OVERRIDE_SECTION_NAME:
+            # Update the roots list
+            old_roots = item['value']['selector']['roots']
+            logger.info(f"Number of validated tests before update {len(old_roots)}")
+            for old_root in old_roots:
+                if old_root not in new_roots:
+                    if not force_override:
+                        raise Exception(f"Detected removal of existing test '{old_root}'. Please use --force-override to proceed")
+                    else:
+                        logger.info(f"Removing existing test {old_root}")
+            logger.info(f"Number of validated tests after  update {len(new_roots)}")
+            new_roots.sort()
+            item['value']['selector']['roots'] = new_roots
+            break
+    else:
+        logger.error(f"Section '{OVERRIDE_SECTION_NAME}' not found.")
+        return
+
+    # Write the updated data back to the YAML file
+    with open(viewless_override_file, 'w') as file:
+        yaml.safe_dump(overrides, file, default_flow_style=False)
+
+def replace_string_in_file(file_path, old_string, new_string):
+    with open(file_path, 'r') as file:
+        file_data = file.read()
+
+    # Replace the target string
+    file_data, num_mod = re.subn(old_string, new_string, file_data)
+    if num_mod:
+        logging.info(f"Replaced text in file {file_path}")
+    with open(file_path, 'w') as file:
+        file.write(file_data)
+
+def replace_string_in_folder(folder_path, old_string, new_string):
+    for root, _, files in os.walk(folder_path):
+        for file_name in files:
+            file_path = os.path.join(root, file_name)
+            replace_string_in_file(file_path, old_string, new_string)
+
+def setup_logging(verbose):
+    logging.basicConfig(level=logging.DEBUG if verbose else logging.INFO)
+
+@click.group()
+@click.option('-v', '--verbose', 'verbose', is_flag=True, show_default=True, default=False, help='Enable debug logs.')
+@click.option(
+        '--mdb-repo',
+        default=os.getenv('MDB_REPO', ''), show_default=True,
+        type=click.Path(exists=True, file_okay=False, dir_okay=True, writable=True, readable=True),
+        help='Path to mongoDB repository')
+def viewless_suites(verbose, mdb_repo):
+    """
+    helper utility to operate on viewless timseries suites
+    """
+    setup_logging(verbose)
+    global MDB_REPO
+    MDB_REPO = mdb_repo
+
+@viewless_suites.command()
+def only_validated_tests():
+    """
+    Enable only validated tests
+    """
+    viewless_suites_folder = os.path.join(MDB_REPO, MAPPING_SUITES_FOLDER)
+    replace_string_in_folder(viewless_suites_folder, 'all_timeseries_tests_selector', 'only_validated_timeseries_tests_selector')
+
+@viewless_suites.command()
+def enable_all_tests():
+    """
+    Enable all tests in viewless suites
+    """
+    viewless_suites_folder = os.path.join(MDB_REPO, MAPPING_SUITES_FOLDER)
+    replace_string_in_folder(viewless_suites_folder, 'only_validated_timeseries_tests_selector', 'all_timeseries_tests_selector')
+
+@viewless_suites.command()
+@click.option(
+        '--tests-results',
+        'tests_results_path',
+        default=os.getenv('TESTS_RESULTS', ''), show_default=True,
+        type=click.Path(exists=True, file_okay=True, dir_okay=False, writable=False, readable=True),
+        help='Path to the folder')
+@click.option('--force-override', is_flag=True, show_default=True, default=False, help='Force override of validated tests even if it will cause removal of existing tests.')
+def update_validated_tests(tests_results_path, force_override):
+    """
+    Enable all tests in viewless suites
+    """
+    with open(tests_results_path, 'r') as file:
+        tests_results = json.load(file)
+
+    succeeded_tests = []
+    for exec_stats in tests_results:
+        test_name = exec_stats['test_name']
+        if exec_stats['num_failed'] == 0:
+            succeeded_tests.append(test_name)
+            continue
+        logger.debug(f'Skipping failed test {test_name}')
+
+    for test_name in succeeded_tests:
+        logger.info(f'Test succeeded {test_name}')
+
+    update_validated_viewless_tests(succeeded_tests, force_override)
+
+
+if __name__ == "__main__":
+    viewless_suites()
